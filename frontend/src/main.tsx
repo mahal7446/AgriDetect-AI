9f1943d2d762389660a5e4bb543dfedee8996edd
mahal7446 <mahalingm0@gmail.com>
2026-02-26 17:34:34 +0530

    connected to analytics

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,7 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import ReactGA from "react-ga4";
 
+ReactGA.initialize("G-V5JEBWT8RL");
 createRoot(document.getElementById("root")!).render(<App />);
